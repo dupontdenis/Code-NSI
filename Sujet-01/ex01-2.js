a2166c7e9e1ab9ea6b9db510c9159763fc25f7be
Super Dupont <dupontdenis@users.noreply.github.com>
2023-04-18 10:27:17 +0200

    Update ex01-2.js

--- Sujet-01/ex01-2.js
+++ Sujet-01/ex01-2.js
@@ -1,19 +1,4 @@
-// On s’intéresse à un algorithme récursif qui permet de rendre la monnaie à partir d’une
-// liste donnée de valeurs de pièces et de billets.
 
-// Le système monétaire est donné sous
-// forme d’une liste `pieces=[100, 50, 20, 10, 5, 2, 1]`.
-// (on supposera qu’il n’y a
-// pas de limitation quant à leur nombre).
-
-// On cherche à donner la liste de pièces à rendre
-// pour une somme donnée en argument.
-// Compléter le code Python ci-dessous de la fonction `rendu_glouton` qui implémente cet
-// algorithme et renvoie la liste des pièces à rendre.
-// >>>rendu_glouton(68,[],0)
-// [50, 10, 5, 2, 1]
-// >>>rendu_glouton(291,[],0)
-// [100, 100, 50, 20, 20, 1
 
 const Pieces = [100, 50, 20, 10, 5, 2, 1];
 const rendu_glouton = function (arendre, solution = [], i = 0) {
