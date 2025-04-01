16cb0d865dbed3ea1a5e00610c576c06450a3f7e
Super Dupont <dupontdenis@users.noreply.github.com>
2025-04-01 16:23:51 +0200

    Update ex11-1.js
    add dico

--- Sujet-11/ex11-1.js
+++ Sujet-11/ex11-1.js
@@ -17,3 +17,27 @@ const recherche2 = (array, val) => {
     return array.indexOf(val);
 }
 console.log(recherche2([2, 3, 4, 5, 6], -2))
+
+//dicotomie
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+// Usage examples
+console.log(binarySearch([2, 3, 4, 5, 6], 5)); // 3
+console.log(binarySearch([2, 3, 4, 6, 7], 5)); // -1
